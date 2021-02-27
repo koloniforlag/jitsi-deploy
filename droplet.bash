7037fbf1c093ee3f5b167148a81ca345aded99ae
Victor Andreasson <victor.andreasson@fripost.org>
2021-02-27 22:22:17 +0100

    Older jq without any arguments misinterpreted the Bash pipe

--- droplet.bash
+++ droplet.bash
@@ -107,7 +107,7 @@ get_float_ip() {
 # Get any unused floating IP
 curl_cmd -X GET \
   "https://api.digitalocean.com/v2/floating_ips" |
-  jq |
+  jq '' |
   grep -B1 '"droplet": null' |
   grep -o -E '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
 }
