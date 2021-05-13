4fe63fde4adf327627257ec7d763c2fd1f21afe7
Victor Andreasson <victor.andreasson@fripost.org>
2021-05-14 01:04:39 +0200

    Testing finished

--- jitsi-deploy-debian10.bash
+++ jitsi-deploy-debian10.bash
@@ -36,8 +36,8 @@ sed -i "s,^read EMAIL$,EMAIL=info@${JITSI_ADDRESS}," $LETSENCRYPT_SCRIPT
 while true; do
   curl --silent "${JITSI_ADDRESS}/thisisatest" >/dev/null &&
     grep -q thisisatest /var/log/nginx/access.log && {
-      # Use the Letsencrypt staging CA during tests
-      sed -i 's,certbot certonly,certbot certonly --test-cert,' $LETSENCRYPT_SCRIPT
+      ## Use the Letsencrypt staging CA during tests
+      #sed -i 's,certbot certonly,certbot certonly --test-cert,' $LETSENCRYPT_SCRIPT
       $LETSENCRYPT_SCRIPT
       exit
     }
